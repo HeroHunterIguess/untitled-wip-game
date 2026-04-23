0e66338bb209e1efee6a30eba053f95bfd9f165d
HeroHunter <abramscarter1999@gmail.com>
2026-04-23 15:01:16 -0400

    general improvements

--- scripts/game/grenade.gd
+++ scripts/game/grenade.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 var velocity = Vector2(0,0)
 var is_on_floor = false
+var hit_something = false
 
 const KNOCKBACK = 1000
 const DAMAGE = 60
@@ -15,15 +16,21 @@ func _physics_process(delta):
 	
 	move_and_collide(velocity * delta)
 
+
+func _on_initial_hit_area_entered(area: Area2D) -> void:
+	hit_something = true
+
+
 # explode when it hits an enemy
 func _on_explosion_radius_area_entered(area: Area2D) -> void:
 	# UPDATE THIS TO ENSURE ITS NOT CALLING FUNCTIONS ON ANOTHER GRENADE INSTANCE WHICH ERRORS
-	if !area.is_in_group("player"):
+	if !area.is_in_group("player") && hit_something:
 		area.get_parent().take_damage(DAMAGE)
-	
-	if area.to_local(self.global_position).x >= 0:
-		area.get_parent().take_kb(KNOCKBACK, false)
-	else: 
-		area.get_parent().take_kb(KNOCKBACK, true)
 		
-	queue_free()
+		
+		if area.to_local(self.global_position).x >= 0:
+			area.get_parent().take_kb(KNOCKBACK, false)
+		else: 
+			area.get_parent().take_kb(KNOCKBACK, true)
+			
+		queue_free()
