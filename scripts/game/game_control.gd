5b37bad5e26bfbe248bfb253846dd4ac17312d46
HeroHunter <abramscarter1999@gmail.com>
2026-04-25 17:16:26 -0400

    bruh idek

--- scripts/game/game_control.gd
+++ scripts/game/game_control.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 var start_time = Time.get_unix_time_from_system()-20
 var game_running = true
+var round_bonus_time = 0
+
+var rng = RandomNumberGenerator.new()
 
 const enemy_preload = preload("res://scenes/enemy types/basic_enemy.tscn")
 
@@ -16,8 +19,20 @@ func _ready():
 			var enemy = enemy_preload.instantiate()
 			add_child(enemy) 
 			var screen_width = get_viewport().size.x
+			var left_location = $player.global_position.x-screen_width-50
+			var right_location = $player.global_position.x+screen_width+50
 			
+			# spawn enemy off screen and vary random distance to have minimal overlap bugs
+			if left_location < 0:
+				enemy.global_position.x = right_location + rng.randi_range(0, 48)
+			if right_location > 4000:
+				enemy.global_position.x = left_location - rng.randi_range(0, 48)
 			
+			# set y to a value close to ground
+			enemy.global_position.y = 500
 		
 		# wait arbitrary amount of time then spawn more
-		await get_tree().create_timer(5).timeout
+		await get_tree().create_timer(5+round_bonus_time).timeout
+		
+		# add extra time for each round bc there will be more enemies
+		round_bonus_time += 0.15
--- scripts/objects/health_pickup.gd
+++ scripts/objects/health_pickup.gd
@@ -2,6 +2,10 @@ extends Area2D
 
 const HEALING = 5
 
+func _ready():
+	print("i am health pickup and i exist")
+	print(self.global_position)
+
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
 		data.player_health += HEALING
