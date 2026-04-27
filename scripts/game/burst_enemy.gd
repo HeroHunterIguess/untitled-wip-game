4ebced7fc97909694c1f15eea56f2753f76fdf6f
HeroHunter <abramscarter1999@gmail.com>
2026-04-27 18:29:25 -0400

    create burst enemy script

---
+++ scripts/game/burst_enemy.gd
@@ -0,0 +1,47 @@
+extends "res://scripts/autoloads/enemy_methods.gd"
+
+const JUMP_FORCE = 550
+const SOFT_SPEED_CAP = 290
+
+# low damage as itll have an ability
+const DAMAGE = 2
+
+# updating movement and physics every frame
+func _physics_process(delta: float) -> void:
+	
+	# move roughly towards player but not fully
+	var random_number = rng.randi_range(1, 4)
+	
+	if random_number == 1:
+		if get_tree().root.find_child("player",true,false).to_local(self.global_position).x >= 0:
+			velocity.x = move_toward(velocity.x, SOFT_SPEED_CAP, global.FRICTION * delta)
+		elif get_tree().root.find_child("player",true,false).to_local(self.global_position).x < 0:
+			velocity.x = move_toward(velocity.x, -SOFT_SPEED_CAP, global.FRICTION * delta)
+	else:
+		if get_tree().root.find_child("player",true,false).to_local(self.global_position).x >= 0:
+			velocity.x = move_toward(velocity.x, -SOFT_SPEED_CAP, global.FRICTION * delta)
+		elif get_tree().root.find_child("player",true,false).to_local(self.global_position).x < 0:
+			velocity.x = move_toward(velocity.x, SOFT_SPEED_CAP, global.FRICTION * delta)
+	
+	# MIGHT REMOVE THIS FOR THIS ENEMY TYPE SO IT DOESNT PILE UP THE SAME WAY
+	# jump if at wall/other enemy
+	if is_on_wall() && is_on_floor():
+		velocity.y -= JUMP_FORCE
+	
+	# apply gravity
+	velocity.y += global.GRAVITY * delta
+	if velocity.y > global.TERMINAL_VELOCITY:
+		velocity.y = global.TERMINAL_VELOCITY
+	
+	# update position based on velocity
+	move_and_slide()
+
+# check for death
+func _process(_delta):
+	if (health <= 0):
+		die()
+
+# deal damage to player when touched
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		area.get_parent().take_damage(DAMAGE)
