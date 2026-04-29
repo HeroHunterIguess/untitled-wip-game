13c146ad27a87accf2fc192046e4a65929de7492
HeroHunter <abramscarter1999@gmail.com>
2026-04-29 16:23:05 -0400

    create burst attack & migrate script locations

---
+++ scripts/game/attacks/burst_attack.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+const DAMAGE = 40
+const KNOCKBACK = 1250
+
+func _on_area_entered(area: Area2D) -> void:
+	# damage things in area
+	var object = area.get_parent()
+	if !object.is_in_group("player"):
+		if object.has_method("take_damage"):
+			object.health = object.take_damage(object.health, DAMAGE)
+		
+		if area.to_local(self.global_position).x >= 0:
+			if object.has_method("take_kb"):
+				object.take_kb(KNOCKBACK, false)
+		else: 
+			if object.has_method("take_kb"):
+				object.take_kb(KNOCKBACK, true)
+	
+	await get_tree().create_timer(0.15).timeout
+	queue_free()
