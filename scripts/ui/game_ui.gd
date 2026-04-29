7bc13df1cee194f63af9efd6c2eb5dcaca0d90ea
HeroHunter <abramscarter1999@gmail.com>
2026-04-29 16:46:37 -0400

    improve ui

--- scripts/ui/game_ui.gd
+++ scripts/ui/game_ui.gd
@@ -7,18 +7,28 @@ func _process(_delta):
 	# display ability statuses
 	if data.double_jumps >= 0:
 		$double_jump_text.text = "Double Jump? \n YES"
+		$double_jump_text.label_settings.font_color = Color(0, 1, 0)
 	else:
 		$double_jump_text.text = "Double Jump? \n NO"
+		$double_jump_text.label_settings.font_color = Color(1, 0, 0)
 	
 	if data.dash_timer <= 0:
 		$dash_text.text = "Dash? \n YES"
+		$dash_text.label_settings.font_color = Color(0, 1, 0)
 	else:
 		$dash_text.text = "Dash? \n NO"
+		$dash_text.label_settings.font_color = Color(1, 0, 0)
 	
 	if data.slam_timer <= 0:
 		$slam_text.text = "Slam? \n YES"
+		$slam_text.label_settings.font_color = Color(0, 1, 0)
 	else:
 		$slam_text.text = "Slam? \n NO"
+		$slam_text.label_settings.font_color = Color(1, 0, 0)
 	
-	#if data.burst_timer <= 0:
-		
+	if data.burst_timer <= 0:
+		$burst_text.text = "Burst? \n YES"
+		$burst_text.label_settings.font_color = Color(0, 1, 0)
+	else:
+		$burst_text.text = "Burst? \n NO"
+		$burst_text.label_settings.font_color = Color(1, 0, 0)
