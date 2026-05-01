2e8dcb9a3bb566729a48892a353b756ab0b2f7e2
HeroHunter <abramscarter1999@gmail.com>
2026-04-30 21:11:02 -0400

    update enemy burst ability

--- scripts/game/burst_enemy.gd
+++ scripts/game/burst_enemy.gd
@@ -55,7 +55,7 @@ const burst_preload = preload("res://scenes/attacks/enemy_burst_attack.tscn")
 
 # spawn in burst attack if player is near
 func _on_burst_range_area_entered(area: Area2D) -> void:
-	if burst_timer <= 0 && area.get_parent().is_in_group("player"):
+	if burst_timer <= 0 && area.get_parent().is_in_group("player") && not data.slamming:
 		burst_timer = BURST_COOLDOWN
 		
 		var burst = burst_preload.instantiate()
