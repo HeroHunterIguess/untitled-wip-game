5e12c221fec820dac8868b25c1cf8a54dc540cf8
HeroHunter <abramscarter1999@gmail.com>
2026-05-07 16:53:51 -0400

    setup orb

---
+++ scripts/game/attacks/orb.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+const DAMAGE = 55
+const KNOCKBACK = 1250
+var checking = true
+
+func _process(_delta):
+	checking = true
+	
+	await get_tree().create_timer(0.5).timeout
+
+func _on_area_entered(area: Area2D) -> void:
+	# damage things in area
+	var object = area.get_parent()
+	if !object.is_in_group("player") && !area.is_in_group("range_detection"):
+		if object.has_method("take_damage"):
+			object.health = object.take_damage(object.health, DAMAGE)
+		
+		if area.to_local(self.global_position).x >= 0:
+			if object.has_method("take_kb"):
+				object.take_kb(KNOCKBACK, false)
+		else: 
+			if object.has_method("take_kb"):
+				object.take_kb(KNOCKBACK, true)
