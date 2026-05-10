8414df9d53c9f0b5d97ce91dd9c0944deaf8dbd8
HeroHunter <abramscarter1999@gmail.com>
2026-05-09 20:39:17 -0400

    adjust damage orb

--- scripts/game/attacks/orb.gd
+++ scripts/game/attacks/orb.gd
@@ -7,9 +7,10 @@ const COOLDOWN = 0.45
 var checking = true
 
 func _process(_delta):
-	checking = true
-	
 	await get_tree().create_timer(COOLDOWN).timeout
+	
+	checking = true
 
 func _on_area_entered(area: Area2D) -> void:
 	deal_damage(area, DAMAGE, KNOCKBACK)
+	checking = false
